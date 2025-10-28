e21cdc83ad92fa58175b4ca6ae3e547c4b85f020
mahmed abdallh <medotatch124@gmail.com>
2025-10-28 19:43:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -383,3 +383,4 @@ if st.button("عرض الحالة"):
 
 if st.session_state.get("show_results", False) and all_sheets:
     check_machine_status(st.session_state.card_num, st.session_state.current_tons, all_sheets)
+
